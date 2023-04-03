abecec04becc3b589e2935fd8b9ef835114575ab
ShraddhaBidwe <shraddhabidve123@gmail.com>
2023-04-03 12:22:14 +0530

    cart changes

--- src/component/Cart.js
+++ src/component/Cart.js
@@ -1,89 +1,38 @@
 import React from "react";
-import './Cart.css';
-import { CgShoppingCart } from 'react-icons/cg';
-import { AiOutlinePlus } from 'react-icons/ai';
-import { AiOutlineMinus } from 'react-icons/ai';
-import { RiDeleteBin5Fill } from 'react-icons/ri';
 
+import './Cart.css';
+import Item from "./Item";
+import { Scrollbars } from 'react-custom-scrollbars-2';
+import { CgShoppingCart } from 'react-icons/cg';
+import { BiArrowBack} from 'react-icons/bi';
 
 
 
 const Cart = () => {
 
+    
+
     return(
         <div className="main-page">
         <div className="header">
-        <a href="/continue shopping"><h4>Continue Shopping</h4></a>
-            <div className="cart-icon"><button><CgShoppingCart /></button></div>
+            <div>
+            <BiArrowBack className="backarrow" />
+            <h4>Continue Shopping</h4>
+            </div>
+            <div>
+               <CgShoppingCart  className="cart-icon"/>
+            </div>
         </div>
         <hr />
-        <div className="address">
-            <p>Deliver to: <b>Pune-411043</b></p>
-            <button className="change-btn">Change</button>
-        </div>
-        <hr />
+        
         <div className="cart-section">
             <h2>Shopping Cart</h2>
             <div className="shopped-items">
                 <div className="items-container">
-                    <div className="product-info">
-                    <div className="item-img">
-                        <img src="https://m.media-amazon.com/images/I/41IcuNkyrdL._SX300_SY300_QL70_FMwebp_.jpg" alt="image"/>
-                    </div>
-                    <div className="item-details">
-                        <h3>Samsung M04</h3>
-                    </div>
-                    <div className="quantity">
-                        <button><AiOutlineMinus /></button>
-                        <input type="text" placeholder="1"></input> 
-                        <button><AiOutlinePlus /></button>                 
-                    </div>
-                    <div className="item-price">
-                        <h4>₹4000</h4>
-                    </div>
-                    <div className="save-later"><button className="save-later-btn">Save For Later</button></div>
-                    <div className="delete-item"><RiDeleteBin5Fill /></div>
-                </div>
-                
-                <hr />
-                <div className="product-info">
-                    <div className="item-img">
-                        <img src="https://m.media-amazon.com/images/I/718XDEWTPIL._UL1500_.jpg" alt="image"/>
-                    </div>
-                    <div className="item-details">
-                        <h3>Samsung M04</h3>
-                    </div>
-                    <div className="quantity">
-                        <button><AiOutlineMinus /></button>
-                        <input type="text" placeholder="1"></input> 
-                        <button><AiOutlinePlus /></button>                 
-                    </div>
-                    <div className="item-price">
-                        <h4>₹2000</h4>
-                    </div>
-                    <div className="save-later"><button className="save-later-btn">Save For Later</button></div>
-                    <div className="delete-item"><RiDeleteBin5Fill /></div>
-                </div>
-                <hr />
-                <div className="product-info">
-                    <div className="item-img">
-                        <img src="https://m.media-amazon.com/images/I/31NVItwqOnL._SX300_SY300_QL70_FMwebp_.jpg" alt="image"/>
-                    </div>
-                    <div className="item-details">
-                        <h3>Samsung M04</h3>
-                    </div>
-                    <div className="quantity">
-                        <button><AiOutlineMinus /></button>
-                        <input type="text" placeholder="1"></input> 
-                        <button><AiOutlinePlus /></button>                 
-                    </div>
-                    <div className="item-price">
-                        <h4>₹4000</h4>
-                    </div>
-                    <div className="save-later"><button className="save-later-btn">Save For Later</button></div>
-                    <div className="delete-item"><RiDeleteBin5Fill /></div>
-                </div>
-                <hr />
+                <Scrollbars>
+                    <Item />
+                </Scrollbars>
+
                 </div>
             </div>
         </div>
