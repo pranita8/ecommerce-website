e6d85baa32417f62f20a3ca68f75041a369a9c39
ShraddhaBidwe <shraddhabidwe123@gmail.com>
2023-04-05 10:59:34 +0530

    CART PAGE CHANGE

--- src/component/Cart.js
+++ src/component/Cart.js
@@ -39,6 +39,7 @@ const Cart = () => {
                         })
                     }
                     
+                               
                 </Scrollbars>
 
                 </div>
