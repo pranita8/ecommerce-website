7dd79d4e1b18c4a68312e753f3ddcf95788dce06
pranita8 <pranitamore842@gmail.com>
2023-04-05 11:34:51 +0530

    input and app.js changes

--- src/App.js
+++ src/App.js
@@ -1,31 +1,20 @@
-
 import './App.css';
 import Login from './component/Login';
 import Signup from './component/Signup';
 import Slider from './component/Slider';
 import Footer from './component/Footer';
+import Product from './component/Product';
 import Header from './component/Header';
 import List from './component/List';
-import Product from './component/Product';
-
-// import Card from './component/Card';
-// import ViewAll from './component/ViewAll';
-
-import Card from './component/Card';
-import ViewAll from './component/ViewAll';
 import { Route, Routes } from 'react-router-dom';
 import AllItems from './component/AllItems';
 
 
 
 
-
-
 // slider array images
 function App() {
-
   const slides = [
-    { url: "https://www.compareraja.in/blog/wp-content/uploads/2014/01/flipkart-book-offers-1440x564_c.jpg"},
     { url: "https://www.compareraja.in/blog/wp-content/uploads/2014/01/flipkart-book-offers-1440x564_c.jpg" },
     { url: "https://i.pinimg.com/originals/b8/ce/12/b8ce12af4e594bcb26b8f55b0377dad4.jpg" },
     { url: "https://images.indianexpress.com/2020/11/Untitled-design-93.jpg" },
@@ -36,10 +25,8 @@ function App() {
 
   return (
     <div className="App">
+  
 
-
-        {/* <ViewAll/> */}
-       
       <Routes>
         {/* home page */}
         <Route path='/' element={
@@ -51,20 +38,14 @@ function App() {
             <Product />
             <Footer />
           </>} />
-          <Route path='list' element={<>
-          <Header/>
-          <List />
-          <Footer/>
-          </>}></Route>
         <Route path='/login' element={<Login />} />
         <Route path='/signup' element={<Signup />} />
         <Route path='/allItems' element={<AllItems/>}/>
-      </Routes>
+        <Route path='/signup' element={<AllItems />} />
 
-    </div>
+      </Routes>
+   </div>
   );
 }
 
 export default App;
-
-
